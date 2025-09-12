9e9772158a2625c78984611673b56933e6c647d6
CerealThread <teleporter2145@gmail.com>
2025-09-12 12:35:27 -0400

    Added diffuse lighting

--- src/camera.h
+++ src/camera.h
@@ -8,6 +8,7 @@ class camera {
     double      aspect_ratio = 1.0;  // Ratio of image width over height
     int         image_width  = 100;  // Rendered image width in pixel count
     int    samples_per_pixel = 10;
+    int    max_depth         = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -20,16 +21,9 @@ class camera {
                 color pixel_color(0,0,0);
                 for (int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
-
-                // auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                // auto ray_direction = pixel_center - center;
-                // ray r(center, ray_direction);
-
-                // color pixel_color = ray_color(r, world);
-                // write_color(std::cout, pixel_color);
             }
         }
 
@@ -85,11 +79,15 @@ class camera {
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if (depth <= 0)
+            return color(0,0,0);
+
         hit_record rec;
 
         if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+            vec3 direction = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
